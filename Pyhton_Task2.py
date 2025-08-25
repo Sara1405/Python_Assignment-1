1297f38bdda257faa6df9fc34cacf10e2001032c
Sara <sara786siddique@gmail.com>
2025-08-25 13:02:30 +0530

    Add files via upload

---
+++ Pyhton_Task2.py
@@ -0,0 +1,10 @@
+# Task 2- Create a Personalized Greeting
+
+first_name = input('Enter your first name: ')
+last_name = input('Enter your second name: ')
+
+# First name and Last name
+first_name = "Sara"
+last_nmae = "Siddique"
+
+print("Hello, Sara Siddique! Welcome to the Python Program.")
---
+++ Python_Task1.py
@@ -0,0 +1,31 @@
+# Task 1- Perform Basic Mathematical Operations.
+
+a = input('Enter the first number: ')
+b = input('Enter the second number: ')
+
+# Numbers
+a = 5
+b = 10
+
+
+# Addition
+addition = a + b
+
+# Subtraction
+subtraction = a - b
+
+# Multiplication
+multiplication = a * b
+
+# Division
+division = a / b
+
+
+# Print result
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+
+
+
